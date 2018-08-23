628179e6db7d1a5598441700852179c9b0c99c17
Seraph <it@ascendantcn.com>
2018-08-23 13:56:08 +0800

    0823-1

---
+++ CodeRecord/Controllers/CodeListsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CodeRecord.Models;
+
+namespace CodeRecord.Controllers
+{
+    public class CodeListsController : Controller
+    {
+        private CodeListContext db = new CodeListContext();
+
+        // GET: CodeLists
+        public ActionResult Index()
+        {
+            return View(db.Codes.ToList());
+        }
+
+        // GET: CodeLists/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CodeList codeList = db.Codes.Find(id);
+            if (codeList == null)
+            {
+                return HttpNotFound();
+            }
+            return View(codeList);
+        }
+
+        // GET: CodeLists/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CodeLists/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,CodeName,Code,Readmd,CreateTime,UpdateTime")] CodeList codeList)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Codes.Add(codeList);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(codeList);
+        }
+
+        // GET: CodeLists/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CodeList codeList = db.Codes.Find(id);
+            if (codeList == null)
+            {
+                return HttpNotFound();
+            }
+            return View(codeList);
+        }
+
+        // POST: CodeLists/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,CodeName,Code,Readmd,CreateTime,UpdateTime")] CodeList codeList)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(codeList).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(codeList);
+        }
+
+        // GET: CodeLists/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CodeList codeList = db.Codes.Find(id);
+            if (codeList == null)
+            {
+                return HttpNotFound();
+            }
+            return View(codeList);
+        }
+
+        // POST: CodeLists/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CodeList codeList = db.Codes.Find(id);
+            db.Codes.Remove(codeList);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CodeRecord/Models/CodeList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeRecord.Models
+{
+    [Table("CodeList")]
+    public class CodeList
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("代码主题")]
+        public string CodeName { get; set; }
+
+        [DisplayName("详细内容")]        
+        public string Code { get; set; }
+
+        [DisplayName("说明信息")]
+        public string Readmd { get; set; }
+
+        [DisplayName("创建时间")]
+        public DateTime CreateTime { get; set; }
+
+        [DisplayName("修改时间")]
+        public DateTime UpdateTime { get; set; }
+    }
+
+    public class CodeListContext : DbContext
+    {
+        public CodeListContext() : base("CodeConn") { }
+
+        public DbSet<CodeList> Codes { get; set; }
+    }
+}
